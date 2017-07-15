86d38665c9a0401598aa6a439c4c8e450133dc59
jacobdlewis <jacobdlewis@gmail.com>
2017-07-15 02:22:53 -0500

    Added activities_controller tests.

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -5,17 +5,55 @@ RSpec.describe Api::ActivitiesController, :type => :controller do
     before(:each) do
       get :index
     end
-
     it "responds successfully" do
       expect(response).to be_success
     end
-
     it "responds with a HTTP 200 status code" do
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status 200
     end
-
     it "responds with JSON content" do
       expect(response.content_type).to eq("application/json")
+    end
+  end
+
+  describe "POST #create" do
+    describe "with valid parameters" do
+      before(:each) do
+        post :create, params: { activity: { name: "Meetings" } }
+      end
+      it "responds successfully" do
+        expect(response).to be_success
+      end
+      it "responds with a 201 status code" do
+        expect(response).to have_http_status 201
+      end
+    end
+    describe "with invalid parameters" do
+      before(:each) do
+        post :create, params: { activity: { name: "" } }
+      end
+      it "responds with failure" do
+        expect(response).to be_failure
+      end
+      it "responds with a 422 status code" do
+        expect(response).to have_http_status 201
+      end
+    end
+
+  end
+
+  describe "PUT #update" do
+    describe "with valid parameters" do
+      before(:each) do
+        activity = create(:activity)
+        put :update, params: { id: activity.id, activity: { name: "Programming" } }
+      end
+      it "responds successfully" do
+        expect(response).to be_success
+      end
+      it "responds with a 200 status code" do
+        expect(response).to have_http_status 200
+      end
     end
   end
 end
